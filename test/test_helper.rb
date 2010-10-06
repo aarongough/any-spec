e7260d020a8cc11d08aa8f5eb45a6d6cfe75872b
aarongough <aaron@aarongough.com>
2010-10-06 18:10:47 -0400

    Added bmore of basic project structure.

---
+++ test/test_helper.rb
@@ -0,0 +1,12 @@
+$test = true
+
+require 'rubygems'
+require 'test/unit'
+
+require_files = []
+require_files << File.join(File.dirname(__FILE__), '..', 'lib',  'any-spec.rb')
+require_files.concat Dir[File.join(File.dirname(__FILE__), 'setup', '*.rb')]
+
+require_files.each do |file|
+  require File.expand_path(file)
+end
