1e8e65a5834b10859c45143482c284025f9bb276
aarongough <aaron@aarongough.com>
2010-10-09 16:35:19 -0400

    Fixed pass percentage reporting, it was inverted.

--- lib/any-spec/test-runner.rb
+++ lib/any-spec/test-runner.rb
@@ -50,7 +50,7 @@ module AnySpec
         message "In file: " + test_case.path.gsub(File.split(@test_specification_file)[0], "") + "\n"
         message test_case.message + "\n\n"
       end
-      pass_rate = format("%.2f",((failed_tests.length.to_f) / @test_cases.length) * 100)
+      pass_rate = format("%.2f",(100.0 - ((failed_tests.length.to_f) / @test_cases.length) * 100))
       message "#{@test_cases.length} tests, #{assertions} assertions, #{failed_tests.count} failures, #{pass_rate}% pass rate\n\n"
       return @test_cases
     end
